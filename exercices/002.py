b5c6ab82c5c0f4eccf0f09e1121101061c7a1b85
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2021-10-19 20:16:46 +0200

    2

---
+++ exercices/002.py
@@ -0,0 +1,25 @@
+from libs.PyxelArt import *
+
+a = 2
+b = a
+c = 1
+
+for loop in range(5) :
+    if a != 4 :
+        colorier(a,1)
+    else :
+        colorier(a,2)
+    a = a + 1
+    
+for loop in range(3) :
+    colorier(1,b)
+    colorier(7,b)
+    b = b + 1
+
+colorier(4,7)
+for loop in range(2) :
+    colorier(4-c,7-c)
+    colorier(4+c,7-c)
+    c = c + 1
+    
+ss()
